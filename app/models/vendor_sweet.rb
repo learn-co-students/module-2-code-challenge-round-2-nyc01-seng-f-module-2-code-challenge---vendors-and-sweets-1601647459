5765ff4c32ff1b2d5bde6f6c34a83aca1d56174c
D1yr <alexander.m.sahli@gmail.com>
2020-10-02 11:31:49 -0400

    7th deliverable finished

--- app/models/vendor_sweet.rb
+++ app/models/vendor_sweet.rb
@@ -1,5 +1,6 @@
 class VendorSweet < ApplicationRecord
     belongs_to :vendor 
     belongs_to :sweet
-    
+    validates :price, numericality: {greater_than: 0}
+    validates :price, presence: true
 end
