3412b8d4817e5990c65a3a01310951f52b9c9982
D1yr <alexander.m.sahli@gmail.com>
2020-10-02 10:52:39 -0400

    second deliverable done

--- app/controllers/vendor_sweets_controller.rb
+++ app/controllers/vendor_sweets_controller.rb
@@ -1,2 +1,20 @@
 class VendorSweetsController < ApplicationController
+    def new
+        @vendorsweet = VendorSweet.new
+        @sweets = Sweet.all
+        @vendors = Vendor.all
+    end
+    def create
+        vendorsweet = VendorSweet.create(vs_params)
+        if vendorsweet.valid?
+            redirect_to vendor_path(vendorsweet.vendor_id)
+        else
+            flash[:myerrors] = vendorsweet.errors.full_messages
+            redirect_to new_vendor_sweet_path
+        end
+    end
+    private
+    def vs_params
+        params.require(:vendor_sweet).permit(:vendor_id, :sweet_id, :price)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :vendor_sweets
-  resources :sweets 
-  resources :vendors 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  get '/sweets', to: "sweets#index", as: "sweets"
+  get '/vendors', to: "vendors#index", as: "vendors"
+  get '/vendors/:id', to: "vendors#show", as: "vendor"
 
 end
